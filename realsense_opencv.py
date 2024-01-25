6382a4b26bbb9207bcd7277007fef4c77d802787
TaigaAnko <antfgxgres@gmail.com>
2024-01-26 08:53:33 +0900

    appling error handling

--- realsense_opencv.py
+++ realsense_opencv.py
@@ -8,6 +8,11 @@ class NotDepthFrame(Exception):
         self.message = message
         super().__init__(self.message)
 
+class NotColorFrame(Exception):
+    def __init__(self, message: str) -> None:
+        self.message = message
+        super().__init__(self.message)
+
 
 class RealSense:
     def __init__(self, height=480, width=640) -> None:
@@ -17,23 +22,28 @@ class RealSense:
         config.enable_stream(rs.stream.depth, width, height, rs.format.z16, 30)
 
         self.pipeline = rs.pipeline()
-        self.pipeline.start(config)
-
+        try:
+            self.pipeline.start(config)
+        except RuntimeError:
+            print("RuntimeError")
+            exit()
         # Alignオブジェクト生成
         align_to = rs.stream.color
         self.align = rs.align(align_to)
 
     def main(self):
+        # 画角調整
+        aligned_frames = self.align.process(self.frames)
+
         try:
-            # 画角調整
-            aligned_frames = self.align.process(self.frames)
-            # 深度フレームを取得
+            # カラーフレーム取得
             color_frame = aligned_frames.get_color_frame()
+            # 深度フレームを取得
             depth_frame = aligned_frames.get_depth_frame()
             if not depth_frame:
                 raise NotDepthFrame("深度フレームが取得できませんでした。")
             if not color_frame:
-                raise
+                raise NotColorFrame("カラーフレームが取得できませんでした。")
 
             # imageをnumpy arrayに
             color_image = np.asanyarray(color_frame.get_data())
@@ -53,9 +63,11 @@ class RealSense:
 
         except NotDepthFrame:
             print("深度フレームが取得できていません。")
+            exit()
 
-        except not color_frame:
+        except NotColorFrame:
             print("カラーフレームが取得できていません。")
+            exit()
 
         while True:
             try:
